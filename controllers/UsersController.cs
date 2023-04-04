421c83d47124d2c5f4a381c5c2a0b72d3137f125
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-04-04 11:07:19 -0300

    refactoring

--- controllers/UsersController.cs
+++ controllers/UsersController.cs
@@ -5,73 +5,72 @@ using MyBlog_API.models;
 using MyBlogAPI.context;
 using MyBlogAPI.services;
 
-namespace MyBlog_API.controllers
+namespace MyBlog_API.Controllers;
+
+[ApiController]
+[Route("api/users")]
+public class UserController : Controller
 {
-    [ApiController]
-    [Route("api/users")]
-    public class UserController : Controller
+    private readonly AppDbContext _context;
+
+    public UserController(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public UserController(AppDbContext context)
+    [EnableCors]
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> UserLogin(ToLoginUser? user)
+    {
+        if (user is null)
         {
-            _context = context;
+            return BadRequest(new {message = "User is null."});
         }
 
-        [EnableCors]
-        [HttpPost("login")]
-        public async Task<ActionResult<string>> UserLogin(ToLoginUser? user)
+        var dbUser = await _context.Users!.FirstOrDefaultAsync(p => p.User == user.User);
+
+        if (dbUser is null)
         {
-            if (user is null)
-            {
-                return BadRequest(new {message = "User is null."});
-            }
-
-            var dbUser = await _context.Users!.FirstOrDefaultAsync(p => p.User == user.User);
-
-            if (dbUser is null)
-            {
-                return NotFound(new {message = "User doesn't exists."});
-            }
-
-            if (!BCrypt.Net.BCrypt.Verify(user.Password, dbUser.Password)) {
-                return Unauthorized(new {message = "Incorrect Password."});
-            }
-
-            return TokenService.GenerateToken(dbUser);
+            return NotFound(new {message = "User doesn't exists."});
         }
 
-        [EnableCors]
-        [HttpPost("register")]
-        public async Task<ActionResult> UserRegister(Users? user)
-        {
-            if (user is null)
-            {
-                return BadRequest(new {message = "User is null."});
-            }
-
-            var dbUser = await _context.Users!.FirstOrDefaultAsync(p => p.User == user.User ||
-                                                                p.UserEmail == user.UserEmail);
-
-            if (dbUser != null && dbUser.User == user.User)
-            {
-                return BadRequest(new {error = "user-already-exist", message = "User already exists."});
-            }
-
-            if (dbUser != null && dbUser.UserEmail == user.UserEmail)
-            {
-                return BadRequest(new {error = "email-already-exist", message = "Email already exists."});
-            }
-
-            user.UserLevel = 1;
-            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
-            user.CreatedAt = DateTime.Now;
-            user.UpdatedAt = DateTime.Now;
-
-            await _context.Users!.AddAsync(user);
-            await _context.SaveChangesAsync();
-
-            return Created("/account-created", new { id = user.Id, user = user.User });
+        if (!BCrypt.Net.BCrypt.Verify(user.Password, dbUser.Password)) {
+            return Unauthorized(new {message = "Incorrect Password."});
         }
+
+        return TokenService.GenerateToken(dbUser);
+    }
+
+    [EnableCors]
+    [HttpPost("register")]
+    public async Task<ActionResult> UserRegister(Users? user)
+    {
+        if (user is null)
+        {
+            return BadRequest(new {message = "User is null."});
+        }
+
+        var dbUser = await _context.Users!.FirstOrDefaultAsync(p => p.User == user.User ||
+                                                            p.UserEmail == user.UserEmail);
+
+        if (dbUser != null && dbUser.User == user.User)
+        {
+            return BadRequest(new {error = "user-already-exist", message = "User already exists."});
+        }
+
+        if (dbUser != null && dbUser.UserEmail == user.UserEmail)
+        {
+            return BadRequest(new {error = "email-already-exist", message = "Email already exists."});
+        }
+
+        user.UserLevel = 1;
+        user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+        user.CreatedAt = DateTime.Now;
+        user.UpdatedAt = DateTime.Now;
+
+        await _context.Users!.AddAsync(user);
+        await _context.SaveChangesAsync();
+
+        return Created("/account-created", new { id = user.Id, user = user.User });
     }
 }
